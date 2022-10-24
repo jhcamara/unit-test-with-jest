cca88a3696e657dd4f355d83d15aa96f3a2952d6
jhcamara <jhcamara@gmail.com>
2022-10-24 17:35:19 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+// we declare the function with the exact name "fromEuroToDollar"
+const fromEuroToDollar = function(valueInEuro){
+    // convert the given valueInEuro to dollars
+    let valueInDollar = valueInEuro * 1.2;
+    //return the dollar value
+    return valueInDollar;
+}
+
+module.exports = { sum, fromEuroToDollar }
